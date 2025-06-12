a4511cc7a29472b2bcb98c41b93c7e6757bd5286
Eishal <eishal.cecos@gmail.com>
2025-06-12 11:41:42 +0500

    p
    p

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,27 +1,25 @@
 const port = 4000;
-const express = require"expres";
+const express = require("express");
 const app = express();
-const mongoose = require"mongoose";
-const jwt = require"jsonwebtoken";
-const multer = require"multer";
-const path = require"path";
-const cors = require"cors";
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-//mongodb+srv://dubai:<db_password>@cluster0.w9lzdxz.mongodb.net/
 
-mongoose.connect("mongodb+srv://dubai:<freshlyfreshfoodtrading>@cluster0.w9lzdxz.mongodb.net/e-commerce");
+mongoose.connect("mongodb+srv://dubai:freshlyfreshfoodtrading@cluster0.w9lzdxz.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get
+app.get("/", (req, res) => {
+  res.send("Express is running");
+});
 
-
-app.listen(port,(error)=>{
-    if(!error){
-        console.log("Server Running");
-    }
-    else{
-        console.log("error"+error);
-    }
-}
-)
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on port " + port);
+  } else {
+    console.log("Error: " + error);
+  }
+});
