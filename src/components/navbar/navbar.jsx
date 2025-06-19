28f8e709aa3731d2bb073ef8c2a9e90547df8a7e
Eishal <eishal.cecos@gmail.com>
2025-06-19 12:19:45 +0500

    p
    p

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 
 const NAVBAR = () => {
   const[menu,setMenu]=useState("shop");
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div className='navbar'>
       <div className='nav-logo'onClick={() => setMenu("Home")}>
@@ -14,39 +15,44 @@ const NAVBAR = () => {
        <img src={logo} alt="" /></Link>
        <p>.</p>
       </div>
-      <ul className="nav-menu">
-  <li onClick={() => setMenu("Home")}>
-    <Link to="/home" className="nav-link">HOME</Link>
-    {menu === "Home" }
-  </li>
-  <li onClick={() => setMenu("About")}>
-    <Link to="/about" className="nav-link">ABOUT</Link>
-    {menu === "About" }
-  </li>
-  <li onClick={() => setMenu("Products")}>
-    <Link to="/products" className="nav-link">PRODUCTS</Link>
-    {menu === "Products" }
-  </li>
-  <li onClick={() => setMenu("ContactUs")}>
-    <Link to="/contactUs" className="nav-link">CONTACT US</Link>
-    {menu === "ContactUs" }
-  </li>
-</ul>
 
-      <div className='shop-button'onClick={() => setMenu("Products")}>
+
+       <div className="hamburger" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+        ☰
+      </div>
+
+
+
+      <ul className={`nav-menu ${mobileMenuOpen ? "open" : ""}`}>
+        <li onClick={() => setMenu("Home")}>
+          <Link to="/home" className="nav-link">HOME</Link>
+        </li>
+        <li onClick={() => setMenu("About")}>
+          <Link to="/about" className="nav-link">ABOUT</Link>
+        </li>
+        <li onClick={() => setMenu("Products")}>
+          <Link to="/products" className="nav-link">PRODUCTS</Link>
+        </li>
+        <li onClick={() => setMenu("ContactUs")}>
+          <Link to="/contactUs" className="nav-link">CONTACT US</Link>
+        </li>
+      </ul>
+
+
+           <div className='shop-button' onClick={() => setMenu("Products")}>
         <Link to="/products" className="nav-link">
-        <button>
-            <p>SHOP NOW</p>
-        </button></Link>
+          <button><p>SHOP NOW</p></button>
+        </Link>
       </div>
 
-      
-      <div className='search' >
-       <img src={search} alt=''/>
+      <div className='search'>
+        <img src={search} alt='search' />
       </div>
+
       <div className='cart' onClick={() => setMenu("Cart")}>
         <Link to="/cart" className="nav-link">
-         <img src={cart} alt=''/></Link>{menu === "Cart" }
+          <img src={cart} alt='cart' />
+        </Link>
       </div>
     </div>
   )
